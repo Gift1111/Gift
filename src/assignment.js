c36eb0d3170faebef24e322d260ef73b2d2d8ac3
God's Gift Ambrose <ambrosegift521@gmail.com>
2022-07-13 17:32:51 +0000

    Challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -1,18 +1,3 @@
-function name(params) {
-  let data = 0;// this is the initial count
-
-  //forloop was needed so as to loop throug the array and get every single number out and add them with them with the data that was equal to 0.
-  for (let index =0; index < params.length; index++){
-    data =params[index] +data;
-  }
-  let Answer = data
-  //console.log(Answer)
-  return Answer)
-}
-
-sumofNumbers([1,23,44,4]);
-
-  console.log(sumofNumbers([1,2,3,4,5,56,6,77,7,87,8,88]));
 // ========================
 // DO NOT EDIT THIS BLOCK
 const assignment = {};
@@ -29,7 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum =0
+
+  for (i=0; i<arrayOfNumbers.length;i++){
+    sum +=arrayOfNumber[i]
+  }
+  return sum;
+  asignment.sumOfNumbers=sumOfNumber;
 }
 // assignment.sumOfNumbers = sumOfNumbers;
 
